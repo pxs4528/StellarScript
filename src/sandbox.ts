c32ebdb530fd371f41a29ee174deffb0c3c9d9fe
jane <jane@janeirl.dev>
2024-10-12 18:15:47 -0500

    Add simulation

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,3 +1,4 @@
+import ace from 'ace-builds'
 import * as ex from 'excalibur'
 import { animManager } from './actors/animation-manager'
 import { Asteroid } from './actors/asteroid'
@@ -5,12 +6,19 @@ import { Console } from './actors/console'
 import { Ship } from './actors/ship'
 import Config from './config'
 import { Images } from './resources'
-import ace from 'ace-builds'
 
 const rootDiv = document.getElementById('rootDiv') as HTMLDivElement
 
 export class Sandbox extends ex.Scene {
 	random = new ex.Random(1337) // seeded random
+
+	deployedCode = `function loop() {
+	// gpio[0]
+	
+}`
+	sinceLastSimulationMs = 0
+	gpio = [0]
+	ship!: Ship
 
 	constructor() {
 		super()
@@ -20,30 +28,56 @@ export class Sandbox extends ex.Scene {
 		// Create the code editor HTML setup when sandbox scene is activated
 		rootDiv.innerHTML = `
 			<section id="sandbox-editor">
-					<div id="editor">function foo(items) {
-    				var x = "All this is syntax highlighted";
-    				return x;
-					}
-					</div>
-				<script>
-					</script>
+				<div id="editor">${this.deployedCode}</div>
 				<footer>
 					<button id="sandbox-pause">Pause</button>
 					<button id="sandbox-deploy">Deploy</button>
 				</footer>
 			</section>
 		`
-		var editor = ace.edit("editor");
-		editor.setTheme("ace/theme/monokai");
-		editor.session.setMode("ace/mode/javascript");
+		const editor = ace.edit('editor')
+		editor.setTheme('ace/theme/tomorrow_night')
+		editor.session.setMode('ace/mode/javascript')
+
 		const pauseButton = document.getElementById('sandbox-pause') as HTMLButtonElement
 		const deployButton = document.getElementById('sandbox-deploy') as HTMLButtonElement
-		deployButton.addEventListener('click', this.onDeploy.bind(this))
+		deployButton.addEventListener('click', () => {
+			const code = editor.getValue()
+			this.deployedCode = code
+		})
 	}
 
 	onDeactivate() {
 		rootDiv.innerHTML = ''
 	}
+
+	onPreUpdate(_engine: ex.Engine, delta: number): void {
+		this.sinceLastSimulationMs += delta
+		if (this.sinceLastSimulationMs >= 500) {
+			this.sinceLastSimulationMs = 0
+			this.simulate()
+		}
+	}
+
+	private simulate() {
+		const gpio = this.gpio
+		Function(`
+			'use strict'
+			const { write, read } = this
+			${this.deployedCode}
+			;loop()
+		`).bind({
+			write(id: number, value: number) {
+				gpio[id] = value
+			},
+			read(id: number) {
+				return gpio[id]
+			},
+		})()
+
+		this.ship.vel = ex.vec(0, gpio[0] ? -100 : 0)
+	}
+
 	onPostUpdate(_engine: ex.Engine, _delta: number): void {
 		// if paused stop the engine
 		const pauseButton = document.getElementById('sandbox-pause') as HTMLButtonElement
@@ -58,18 +92,12 @@ export class Sandbox extends ex.Scene {
 		})
 	}
 
-	onDeploy() {
-		const editor = document.querySelector('#sandbox-editor>textarea') as HTMLTextAreaElement
-		const code = editor.value + `;onUpdate()`
-		const func = new Function(code)
-		func()
-	}
-
 	onInitialize(engine: ex.Engine) {
 		engine.add(animManager)
 
-		const ship = new Ship(engine.halfDrawWidth, 800, 80, 80)
+		const ship = new Ship(engine.halfDrawWidth, 600, 80, 80)
 		engine.add(ship)
+		this.ship = ship
 
 		let asteroidTimer = new ex.Timer({
 			fcn: () => {
